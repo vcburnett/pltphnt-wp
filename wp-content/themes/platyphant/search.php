d82744950ea44d470f51b9cf5421e12b6b5db4de
vcburnett <victorburnett@Victors-iMac-2.local>
2017-01-04 22:13:54 -0500

    Integration almost complete
    - missing slider
    - missing content
    - missing minor texts

--- wp-content/themes/platyphant/search.php
+++ wp-content/themes/platyphant/search.php
@@ -1,53 +1,36 @@
-<?php
-/**
- * The template for displaying search results pages
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<div class="menu-compensation"></div>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section class="content-thin">
 
-		<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+		<header class="page-header">
+			<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
+		</header><!-- .page-header -->
 
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+			/**
+			 * Run the loop for the search to output the results.
+			 * If you want to overload this in a child theme then include a file
+			 * called content-search.php and that will be used instead.
+			 */
+			get_template_part( 'template-parts/content', 'search' );
 
-			// End the loop.
-			endwhile;
+		// End the loop.
+		endwhile;
 
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-			) );
+	// If no content, include the "No posts found" template.
+	else :
+		get_template_part( 'template-parts/content', 'none' );
 
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
+	endif;
+	?>
 
-		endif;
-		?>
+</section>
 
-		</main><!-- .site-main -->
-	</section><!-- .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/platyphant/template-parts/content-search.php
+++ wp-content/themes/platyphant/template-parts/content-search.php
@@ -1,53 +1,14 @@
-<?php
-/**
- * The template part for displaying results in search pages
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
-?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
 
-	<?php twentysixteen_post_thumbnail(); ?>
+	<div class="search-wrapper">
+		<div class="search-thumb">
+			<?php the_post_thumbnail(); ?>
+		</div>
+		<div class="search-text">
+			<h1><?php echo get_the_title(); ?></h1>
+			<p><?php the_excerpt(); ?></p>
+			<a href="<?php the_permalink(); ?>" class="search-cta">View the project</a>
+		</div>
+	</div>
 
-	<?php twentysixteen_excerpt(); ?>
-
-	<?php if ( 'post' === get_post_type() ) : ?>
-
-		<footer class="entry-footer">
-			<?php twentysixteen_entry_meta(); ?>
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-
-	<?php else : ?>
-
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<footer class="entry-footer"><span class="edit-link">',
-				'</span></footer><!-- .entry-footer -->'
-			);
-		?>
-
-	<?php endif; ?>
 </article><!-- #post-## -->
-
